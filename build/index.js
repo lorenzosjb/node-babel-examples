1cf3e3e38e38dc2539df696183e813a1b208ba69
Lorenzo <lorenzo@MacBook-Pro-de-Lorenzo.local>
2023-09-12 16:53:49 -0600

    Added stuff

---
+++ build/index.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var f = [1, 2, 3, 4, 5];
+var values = f.filter(function (x) {
+  return x > 3;
+});
+console.log("Hello world");
+console.log(values);
---
+++ src/index.js
@@ -0,0 +1,5 @@
+const f = [1,2,3,4,5];
+const values = f.filter(x => x > 3);
+
+console.log("Hello world");
+console.log(values);
